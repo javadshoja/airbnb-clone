4b22e44a9f4c82c4b004dd4a32c052a4fc045cba
jbrave <jbraave@gmail.com>
2023-06-09 13:44:05 +0330

    update(Avatar): add user image src

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,13 +1,20 @@
 import Image from 'next/image'
+import { FC } from 'react'
 
-export default function Avatar() {
+type AvatarProps = {
+	src?: string | null | undefined
+}
+
+const Avatar: FC<AvatarProps> = ({ src }) => {
 	return (
 		<Image
 			className='rounded-full'
 			height={30}
 			width={30}
-			src='/images/placeholder.jpg'
+			src={src || '/images/placeholder.jpg'}
 			alt='Avatar'
 		/>
 	)
 }
+
+export default Avatar
