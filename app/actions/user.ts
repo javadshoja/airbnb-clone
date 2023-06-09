4f8765d3907a408a6e1814b0570cb60a1c7a0cd9
jbrave <jbraave@gmail.com>
2023-06-09 16:04:54 +0330

    feat: add user actions

---
+++ app/actions/user.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import prisma from '@/app/libs/prismadb'
+
+import bcrypt from 'bcrypt'
+import { zact } from 'zact/server'
+import { z } from 'zod'
+
+export const registerUser = zact(
+	z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string()
+	})
+)(async ({ name, email, password }) => {
+	const hashedPassword = await bcrypt.hash(password, 12)
+
+	return await prisma.user.create({
+		data: {
+			name,
+			email,
+			hashedPassword
+		}
+	})
+})
