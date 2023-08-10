cb34e4863b3c777ceecd4017a5d08a4bbebf65b9
javadshoja <jbraave@gmail.com>
2023-08-10 13:10:24 +0330

    fix style

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -15,7 +15,7 @@ type NavbarProps = {
 
 const Navbar: FC<NavbarProps> = ({ currentUser }) => {
 	return (
-		<div className='fixed z-10 w-full bg-white shadow-sm'>
+		<div className='fixed top-0 z-10 w-full bg-white shadow-sm'>
 			<div className='border-b py-4'>
 				<Container>
 					<div
