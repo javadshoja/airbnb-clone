028cc7946a3cbd0f9c9bf50b844897c52d47c471
javadshoja <jbraave@gmail.com>
2023-08-10 11:51:08 +0330

    feat: add get listings

---
+++ app/services/listing.ts
@@ -0,0 +1,14 @@
+import { trytm } from '@bdsqqq/try'
+import prisma from '../libs/prismadb'
+
+export async function getListings() {
+	const [listings, error] = await trytm(
+		prisma.listing.findMany({
+			orderBy: { createdAt: 'desc' }
+		})
+	)
+
+	if (error) console.error(error)
+
+	return listings
+}
