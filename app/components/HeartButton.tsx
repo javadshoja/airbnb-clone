ad9d93a98715e99af54ea58f157b1a356fe0edfd
javadshoja <jbraave@gmail.com>
2023-08-10 11:49:57 +0330

    feat: add heart button

---
+++ app/components/HeartButton.tsx
@@ -0,0 +1,43 @@
+import type { SafeUser } from '@/types'
+import { FC } from 'react'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+import { cn } from '../libs/utils'
+
+type HeartButtonProps = {
+	listingId: string
+	currentUser: SafeUser | null
+}
+
+const HeartButton: FC<HeartButtonProps> = ({ listingId, currentUser }) => {
+	const hasFavorited = false
+	const toggleFavorite = () => {}
+
+	return (
+		<div
+			onClick={toggleFavorite}
+			className='
+        relative
+        cursor-pointer
+        transition
+        hover:opacity-80
+      '
+		>
+			<AiOutlineHeart
+				size={28}
+				className='
+          absolute
+          -right-[2px]
+          -top-[2px]
+          fill-white
+        '
+			/>
+			<AiFillHeart
+				size={24}
+				className={cn('fill-neutral-500/70', {
+					'fill-rose-500': hasFavorited
+				})}
+			/>
+		</div>
+	)
+}
+export default HeartButton
