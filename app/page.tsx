6ea44eebe3e5f81d6c6f560f88911eb822e8f5a4
javadshoja <jbraave@gmail.com>
2023-08-10 11:52:43 +0330

    feat: add empty state & listing list

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
-export default function HomePage() {
+import Container from './components/Container'
+import EmptyState from './components/EmptyState'
+import ListingCard from './components/listings/ListingCard'
+import { getListings } from './services/listing'
+import getCurrentUser from './services/user'
+
+export default async function HomePage() {
+	const listings = await getListings()
+	const currentUser = await getCurrentUser()
+
+	if (listings?.length === 0) {
+		return <EmptyState showReset />
+	}
 	return (
-		<main className='text-3xl text-rose-500'>
-			<h1>Hello Airbnb</h1>
-		</main>
+		<Container>
+			<div
+				className='
+					grid
+					grid-cols-1
+					gap-8
+					sm:grid-cols-2
+					md:grid-cols-3
+					lg:grid-cols-4
+					xl:grid-cols-5
+					2xl:grid-cols-6
+					'
+			>
+				{listings?.map(listing => (
+					<ListingCard
+						key={listing.id}
+						listing={listing}
+						currentUser={currentUser}
+					/>
+				))}
+			</div>
+		</Container>
 	)
 }
