01f0d20dc91c6f1a8d4ff2ffe2a1679b0a836a68
javadshoja <jbraave@gmail.com>
2023-08-10 11:47:59 +0330

    update get user

--- app/services/user.ts
+++ app/services/user.ts
@@ -18,5 +18,10 @@ export default async function getCurrentUser() {
 
 	if (!currentUser) return null
 
-	return currentUser
+	return {
+		...currentUser,
+		createdAt: currentUser.createdAt.toISOString(),
+		updatedAt: currentUser.updatedAt.toISOString(),
+		emailVerified: currentUser.emailVerified?.toISOString() || null
+	}
 }
