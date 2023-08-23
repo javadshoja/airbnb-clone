96db7f3f854d7877f97b0a3b09afbff4ad12a1f1
javadshoja <jbraave@gmail.com>
2023-08-23 15:40:33 +0330

    update: add new params to get listings

--- services/listing.ts
+++ services/listing.ts
@@ -1,9 +1,70 @@
 import { trytm } from '@bdsqqq/try'
 import prisma from '../libs/db'
 
-export async function getListings() {
+export type getListingsParams = {
+	userId?: string
+	category?: string
+	startDate?: string
+	endDate?: string
+	guestCount?: number
+	roomCount?: number
+	bathroomCount?: number
+	locationValue?: string
+}
+
+export async function getListings({
+	userId,
+	category,
+	startDate,
+	endDate,
+	guestCount,
+	roomCount,
+	bathroomCount,
+	locationValue
+}: getListingsParams) {
 	try {
+		const query: Record<string, {}> = {}
+
+		if (userId) query.userId = userId
+
+		if (category) query.category = category
+
+		if (guestCount)
+			query.guestCount = {
+				gte: +guestCount
+			}
+		if (roomCount)
+			query.roomCount = {
+				gte: +roomCount
+			}
+		if (bathroomCount)
+			query.bathroomCount = {
+				gte: +bathroomCount
+			}
+
+		if (locationValue) query.locationValue = locationValue
+
+		if (startDate && endDate) {
+			query.NOT = {
+				reservations: {
+					some: {
+						OR: [
+							{
+								endDate: { gte: startDate },
+								startDate: { lte: startDate }
+							},
+							{
+								startDate: { lte: endDate },
+								endDate: { gte: endDate }
+							}
+						]
+					}
+				}
+			}
+		}
+
 		const listings = await prisma.listing.findMany({
+			where: query,
 			orderBy: { createdAt: 'desc' }
 		})
 
