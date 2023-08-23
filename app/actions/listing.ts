bcf816a0036ea1bd775b6354d6ef513e4e768305
javadshoja <jbraave@gmail.com>
2023-08-23 13:42:12 +0330

    feat: add delete lisitng

--- app/actions/listing.ts
+++ app/actions/listing.ts
@@ -5,6 +5,7 @@ import { zact } from 'zact/server'
 import prisma from '@/libs/db'
 import getCurrentUser from '@/services/user'
 import { ListingSchema } from '@/libs/schemas'
+import { z } from 'zod'
 
 export const createListing = zact(ListingSchema)(async ({
 	title,
@@ -18,6 +19,7 @@ export const createListing = zact(ListingSchema)(async ({
 	roomCount
 }) => {
 	const currentUser = await getCurrentUser()
+
 	return await prisma.listing.create({
 		data: {
 			title,
@@ -30,6 +32,19 @@ export const createListing = zact(ListingSchema)(async ({
 			price,
 			roomCount,
 			userId: currentUser?.id!
+		}
+	})
+})
+
+export const deleteListing = zact(z.string())(async listingId => {
+	const currentUser = await getCurrentUser()
+
+	if (!currentUser) throw new Error('User not found')
+
+	return await prisma.listing.deleteMany({
+		where: {
+			id: listingId,
+			userId: currentUser.id
 		}
 	})
 })
