a2a24c3a83720900d41a1a241b1e138a9c04282e
javadshoja <jbraave@gmail.com>
2023-08-23 15:39:19 +0330

    update search

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -1,10 +1,52 @@
 'use client'
 
+import useCountries from '@/hooks/useCountries'
+import useSearchModal from '@/hooks/useSearchModal'
+import { differenceInDays } from 'date-fns'
+import { useSearchParams } from 'next/navigation'
+import { useMemo } from 'react'
 import { BiSearch } from 'react-icons/bi'
 
 export default function Search() {
+	const searchModal = useSearchModal()
+	const params = useSearchParams()
+	const { getByValue } = useCountries()
+
+	const locationValue = params.get('locationValue')
+	const startDate = params.get('startDate')
+	const endDate = params.get('endDate')
+	const guestCount = params.get('guestCount')
+
+	const locationLabel = useMemo(() => {
+		if (locationValue) return getByValue(locationValue)?.label
+
+		return 'Anywhere'
+	}, [getByValue, locationValue])
+
+	const durationLabel = useMemo(() => {
+		if (startDate && endDate) {
+			const start = new Date(startDate)
+			const end = new Date(endDate)
+
+			let diff = differenceInDays(end, start)
+
+			if (diff === 0) diff = 1
+
+			return `${diff} Days`
+		}
+
+		return 'Any Week'
+	}, [startDate, endDate])
+
+	const guestLabel = useMemo(() => {
+		if (guestCount) return `${guestCount} Guests`
+
+		return 'Add Guests'
+	}, [guestCount])
+
 	return (
 		<div
+			onClick={searchModal.onOpen}
 			className='
         w-full
         cursor-pointer
@@ -23,7 +65,7 @@ export default function Search() {
           items-center
           justify-between'
 			>
-				<div className='px-6 text-sm font-semibold'>Anywhere</div>
+				<div className='px-6 text-sm font-semibold'>{locationLabel}</div>
 				<div
 					className='
             hidden
@@ -36,7 +78,7 @@ export default function Search() {
             sm:block
         '
 				>
-					Any Week
+					{durationLabel}
 				</div>
 				<div
 					className='
@@ -50,7 +92,7 @@ export default function Search() {
             text-gray-600
         '
 				>
-					<div className='hidden sm:block'>Add Guests</div>
+					<div className='hidden sm:block'>{guestLabel}</div>
 					<div
 						className='
               rounded-full
